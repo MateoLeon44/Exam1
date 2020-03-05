8b267a5e4d778b92c6997fd8ddefc49c277be19b
MateoLeon <mateoleon127@gmail.com>
2020-03-05 13:41:21 -0500

    Second commit

--- public/javascripts/dbs.js
+++ public/javascripts/dbs.js
@@ -4,6 +4,14 @@ const db = document.querySelector("#dbs");
 
 db.addEventListener("change", () => {
     const dbValue = document.getElementById("dbs").value;
-    console.log("db", dbValue);
-    
+    fetch("/getCollections/" + dbValue).then(res => res.json()).then(mostrar);
 });
+
+const mostrar = colecciones =>{
+    const col = document.getElementById("colecciones")
+    col.style.display = "block";
+    col.innerHTML = "";
+    col.innerHTML = "<option>" + "</option>";
+    console.log(colecciones);
+
+}
